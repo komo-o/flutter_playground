0fd295838cc89d9aca83fe162e021fcf2f57b8d2
kotarokomori <komopro.works@gmail.com>
2025-03-31 23:21:01 +0900

    Qiitaから記事を取得する処理を追加

--- lib/screens/search_screen.dart
+++ lib/screens/search_screen.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http; // httpという変数を通して、httpパッケージにアクセス
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_playground/models/article.dart';
 
 class SearchScreen extends StatefulWidget {
   const SearchScreen({super.key});
@@ -14,5 +18,27 @@ class _SearchScreenState extends State<SearchScreen> {
       appBar: AppBar(title: const Text('Qiita Search')),
       body: Container(),
     );
+  }
+
+  Future<List<Article>> searchQiita(String keyword) async {
+    final uri = Uri.https('qiita.com', '/api/v2/items', {
+      'query': 'title:$keyword',
+      'per_page': '10',
+    });
+
+    final String token = dotenv.env['QIITA_ACCESS_TOKEN'] ?? '';
+
+    final http.Response res = await http.get(
+      uri,
+      headers: {'Authorization': 'Bearer $token'},
+    );
+
+    if (res.statusCode == 200) {
+      // モデルクラスへ変換
+      final List<dynamic> body = jsonDecode(res.body);
+      return body.map((dynamic json) => Article.fromJSON(json)).toList();
+    } else {
+      return [];
+    }
   }
 }
